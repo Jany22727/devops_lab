4c0fa308823db4ca3a0787a45126c189c3dee48c
Yauheniya Rayetskaya <44959828+Jany22727@users.noreply.github.com>
2018-11-19 11:41:36 +0300

    Update t7.py

--- t7.py
+++ t7.py
@@ -1,5 +1,5 @@
-import yaml
 from jinja2 import Template
+import yaml
 
 with open('data.yml') as data_file:
     config_data = yaml.load(data_file)
@@ -11,4 +11,4 @@ template = Template(template_html)
 vhosts_conf = template.render(config_data)
 
 with open('vhosts.conf', 'w') as vhosts_file:
-     vhosts_file.write(vhosts_conf)
+    vhosts_file.write(vhosts_conf)
