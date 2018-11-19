a08f6709a2e1e048ced1eaf92bbcf0f79d9ea4e0
Yauheniya Rayetskaya <44959828+Jany22727@users.noreply.github.com>
2018-11-19 15:41:20 +0300

    Update TestTask2_3.py

--- TestTask2_3.py
+++ TestTask2_3.py
@@ -2,10 +2,13 @@ from unittest import TestCase
 
 import task2_3
 
+
 class TestTask2_3(TestCase):
     def setUp(self):
         """Init"""
     def test_hamming(self):
-        self.assertEqual(task2_3.hamming(5,6), 2)
+        self.assertEqual(task2_3.hamming(5, 6), 2)
+        
     def tearDown(self):
         """Finish"""
+        
